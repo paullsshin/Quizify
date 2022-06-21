8869106efc7e0c94f224721efe8e5f949097a098
paullsshin <paullsshin@hotmail.com>
2022-06-20 21:40:16 -0700

    added to html,css, and js

---
+++ script.js
@@ -0,0 +1,17 @@
+var start = $("#start");
+var game = $("#game");
+var questions = $("#questions");
+var timer = $("#timer");
+var choices = $("#choices");
+var score = $("#score");
+var A = $("#A");
+var B = $("#B");
+var C = $("#C");
+var D = $("#D");
+var score = 0;
+
+var questions = [
+    {
+        question : ""
+    }
+]
